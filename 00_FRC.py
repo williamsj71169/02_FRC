4e19b3de73b9a80d0af6d87c8f3fcc5356e33444
williamsj71169 <58008516+williamsj71169@users.noreply.github.com>
2021-04-07 14:32:58 +1200

    2

--- 00_FRC.py
+++ 00_FRC.py
@@ -17,8 +17,7 @@ def num_check(question, error, num_type):
             print(error)
 
 # main stuff
-get_int = num_check("How many do you need? Please enter an amount more than 0\n", int)
+get_int = num_check("How many do you need?", "Please enter an amount more than 0\n", int)
 
 
-get_cost = num_check("How many does it cost? Please enter an amount more than 0\n", float)
-
+get_cost = num_check("How many does it cost?", "Please enter an amount more than 0\n", float)
