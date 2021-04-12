b9b9bc38a94ed0e7d2bf638a826a9bd2b135610c
williamsj71169 <58008516+williamsj71169@users.noreply.github.com>
2021-04-13 09:22:55 +1200

    rocommended price

--- 05b_recommended_price.py
+++ 05b_recommended_price.py
@@ -1,5 +1,42 @@
 import math
 
 
-# integer that is
+# integer that is more then zero. Takes in custom error message
+def num_check(question, error, num_type):
+    valid = False
+    while not valid:
 
+        try:
+            response = num_type(input(question))
+
+            if response <= 0:
+                print(error)
+
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+
+# rounding function
+def round_up(amount, round_to):
+    return int(math.ceil(amount / round_to)) * round_to
+
+
+# Main stuff here:
+how_many = num_check("How Many Items? ", "Can't be 0", int)
+total = num_check("Total Costs? ", "More than 0", float)
+profit_goal = num_check("Profit Goal? ", "More than 0", float)
+round_to = num_check("Round to nearest? ", "Can't be 0", int)
+
+sales_needed = total + profit_goal
+
+print("Total: ${:.2f}".format(total))
+print("Profit Goal: ${:.2f}".format(profit_goal))
+
+selling_price = sales_needed / how_many
+print("Selling Price (unrounded): ${:.2f}".format(selling_price))
+
+recommended_price = round_up(selling_price, round_to)
+print("Recommended Price: ${:.2f}".format(recommended_price))
