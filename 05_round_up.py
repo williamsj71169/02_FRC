4f9864ec561d337358bdac8fb7763beb37af71de
williamsj71169 <58008516+williamsj71169@users.noreply.github.com>
2021-04-13 09:04:36 +1200

    yesterday stuff

--- 05_round_up.py
+++ 05_round_up.py
@@ -3,7 +3,7 @@ import math
 
 # rounding function
 def round_up(amount, round_to):
-    return int (math.ceil(amount / round_to)) * round_to
+    return int(math.ceil(amount / round_to)) * round_to
 
 
 # Main Stuff
---
+++ 05b_recommended_price.py
@@ -0,0 +1,5 @@
+import math
+
+
+# integer that is
+
